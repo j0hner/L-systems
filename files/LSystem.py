79a0f830ff996173a563479c475441639b77d051
j0hner <honner.jan@gmail.com>
2025-01-14 13:43:45 +0100

    moved all files into files directory

--- files/LSystem.py
+++ files/LSystem.py
@@ -1,12 +1,12 @@
 from types import FunctionType
 
 class LSystem:
-    def __init__(self, rules: dict[str, str], consants:list[str], variables:list[str], initState: str, turtleRules: dict[str, FunctionType] = {}) -> None:
+    def __init__(self, rules: dict[str, str], constants:list[str], variables:list[str], initState: str, turtleRules: dict[str, FunctionType] = {}) -> None:
 
         self.rules = rules
         self.turtleRules = turtleRules
-        self.consts = consants
-        self.vars = variables
+        self.constants = constants
+        self.variables = variables
         self.initState = initState
     def StateGenerator(self, log:bool = False):
         """will yield the next recursion of the system"""
@@ -16,8 +16,8 @@ class LSystem:
         while True:
             yield oldStr
             for char in oldStr:
-                if char not in self.rules.keys() and not char in self.consts and not char in self.vars: raise KeyError(f"there is no rule, constant or variable defining the behavior of '{char}' in this system")
-                if char in self.consts:
+                if char not in self.rules.keys() and not char in self.constants and not char in self.variables: raise KeyError(f"there is no rule, constant or variable defining the behavior of '{char}' in this system")
+                if char in self.constants:
                     newStr += char
                     continue
                 newStr += self.rules[char]
@@ -41,8 +41,8 @@ class LSystem:
         
         for _ in range(n):
             for char in oldStr:
-                if char not in self.rules.keys() and not char in self.consts and not char in self.vars: raise KeyError(f"there is no rule, constant or variable defining the behavior of '{char}' in this system")
-                if char in self.consts:
+                if char not in self.rules.keys() and not char in self.constants and not char in self.variables: raise KeyError(f"there is no rule, constant or variable defining the behavior of '{char}' in this system")
+                if char in self.constants:
                     newStr += char
                     continue
                 newStr += self.rules[char]
@@ -53,5 +53,5 @@ class LSystem:
     
     def DrawSystem(self, n:int):
         for instruction in self.GetState(n):
-            if instruction not in self.turtleRules.keys(): raise KeyError(f"there is no turtle rule defining the behavior of '{instruction}' in this system")
+            if instruction not in self.turtleRules.keys() and not instruction in self.variables and not instruction in self.variables: raise KeyError(f"there is no rule, constant or variable defining the behavior of '{instruction}' in this system")
             self.turtleRules[instruction]()
